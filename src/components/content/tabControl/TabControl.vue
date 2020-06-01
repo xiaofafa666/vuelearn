3de22f209a97da6ea11d87511bbb7bfb98bb45b0
xiaofafa666 <2218298419@qq.com>
2020-06-02 01:46:03 +0800

    首页选项卡切换

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tabcontrol">
+    <div v-for="(item,index) in title" :key="index" class="tabcontrollist">
+      <span class="nocheck" :class="{ischeck: index===currentIndex}" 
+      @click="itemclick(index)">{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    title: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+        currentIndex:0,
+    };
+  },
+  methods:{
+          itemclick(index){
+            //   console.log(index)
+            this.currentIndex = index
+      }
+  },
+  components: {}
+};
+</script>
+
+<style scoped>
+.tabcontrol{
+    display: flex;
+    height: 35px;
+    line-height: 35px;
+}
+.tabcontrol .tabcontrollist{
+    flex: 1;
+    text-align: center;
+}
+.nocheck{
+    font-size: 14px;
+}
+.ischeck{
+    border-bottom: 3px solid var(--color-high-text);
+    /* border-color: red; */
+    color: var(--color-high-text);
+    font-size: 16px;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,14 +3,47 @@
         <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
         <home-swiper :banner="banner"></home-swiper> 
         <homecommend :recommend="recommend"></homecommend>
+        <feature-view></feature-view>
+        <tab-control :title="['精选','流行','爆款']"></tab-control>
+          <ul>
+               <li>列表1</li>
+               <li>列表2</li>
+               <li>列表3</li>
+               <li>列表4</li>
+               <li>列表5</li>
+               <li>列表6</li>
+               <li>列表7</li>
+               <li>列表8</li>
+               <li>列表9</li>
+               <li>列表10</li>
+               <li>列表11</li>
+               <li>列表12</li>
+               <li>列表13</li>
+               <li>列表14</li>
+               <li>列表15</li>
+               <li>列表16</li>
+               <li>列表17</li>
+               <li>列表18</li>
+               <li>列表19</li>
+               <li>列表20</li>
+               <li>列表21</li>
+               <li>列表22</li>
+               <li>列表23</li>
+               <li>列表24</li>
+               <li>列表25</li>
+          </ul>
+
      </div>
 
 </template>
 
 <script>
- import NavBar from 'components/common/navbar/NavBar';
  import HomeSwiper from './childComps/HomeSwiper'
  import Homecommend from './childComps/Homecommend'
+ import FeatureView from './childComps/FeatureView'
+
+ import NavBar from 'components/common/navbar/NavBar'
+ import TabControl from '../../components/content/tabControl/TabControl'
 
  import {getHomeMultiData} from 'network/home.js';
 export default {
@@ -26,7 +59,9 @@ export default {
  components: {
      NavBar,
      HomeSwiper,
-     Homecommend
+     Homecommend,
+     FeatureView,
+     TabControl
  },
  created () {
   //  getHomeMultiData()
@@ -41,9 +76,17 @@ export default {
 </script>
 
 <style scoped>
-  
+     #home{
+          padding-top: 43px;
+     }
 	.home-nav{
 		background-color: var(--color-tint);
 		color: white;
+          position: fixed;
+          left: 0;
+          right: 0;
+          top: 0;
+          z-index: 9;
+          /* margin-bottom: 0px; */
 	}
 </style>
