579b649360b9e92f717a0c09149b0586e3c9fc14
xiaofafa666 <2218298419@qq.com>
2020-06-07 22:46:42 +0800

    商品标题信息及店铺信息展示

--- src/network/detail.js
+++ src/network/detail.js
@@ -8,3 +8,32 @@ export function getDetail(iid){
         }
     })
 }
+
+export class Goods{
+    constructor(itemInfo,columns,services){
+        this.title = itemInfo.title;
+        this.desc = itemInfo.desc;
+        this.oldPrice = itemInfo.oldPrice;
+        this.lowNowPrice = itemInfo.lowNowPrice;
+        this.price = itemInfo.price;
+        this.discountBgColor = itemInfo.discountBgColor;
+        this.discountDesc = itemInfo.discountDesc;
+    
+        this.columns = columns;
+    
+        this.services = services;
+        
+    }
+}
+
+export class Goodsshop{
+    constructor(shopInfo){
+        this.shoplogo = shopInfo.shopLogo;
+        this.shopname = shopInfo.name;
+        this.fans = shopInfo.cFans;
+        this.sells = shopInfo.cSells;
+        this.score = shopInfo.score;
+        this.goodsCount = shopInfo.cGoods;
+        this.shopurl =shopInfo.shopUrl
+    }
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,41 @@
 <template>
   <div class="detail">
-      <detailnavbar ></detailnavbar>
- <detail-swiper :swipperimg="swipperimg" ></detail-swiper>
+      <detailnavbar  class="detailnavbar"></detailnavbar>
+    <scroll class="content1">
+ <detail-swiper :swipperimg="swipperimg" class="swipper"></detail-swiper>
+ <detail-goods :goods="goods"></detail-goods>
+ <detail-goods-shop :goodsshop="goodsshop"></detail-goods-shop>
+    <ul>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+    </ul>
+    </scroll>
   </div>
 </template>
 
 <script>
+import Scroll from "components/common/scroll/Scroll";
 import Detailnavbar from "views/detail/childComps/Detailnavbar";
 import DetailSwiper from "views/detail/childComps/DetailSwiper";
+import DetailGoods from "views/detail/childComps/DetailGoods";
+import DetailGoodsShop from "views/detail/childComps/DetailGoodsshop";
 
-import {getDetail} from "network/detail"
+import {getDetail,Goods,Goodsshop} from "network/detail"
 
 export default {
   name:"Detail",
   data() {
     return {
       iid: null,
-      swipperimg:null
+      swipperimg:null,
+      goods:{},
+      goodsshop:{}
     };
   },
   created() {
@@ -24,17 +43,43 @@ export default {
     this.iid = this.$route.params.iid;
     //获取商品详情
     getDetail(this.iid).then(res=>{
-      this.swipperimg = res.result.itemInfo.topImages
+      const data = res.result
+      this.swipperimg = data.itemInfo.topImages
       console.log(res);
-      
+    //获取商品信息
+      this.goods = new Goods(res.result.itemInfo,res.result.columns,res.result.shopInfo.services)
+    //获取关于店铺信息
+      this.goodsshop = new Goodsshop(res.result.shopInfo)
+
     })
   },
   components: {
+    Scroll,
     Detailnavbar,
-    DetailSwiper
+    DetailSwiper,
+    DetailGoods,
+    DetailGoodsShop,
   }
 };
 </script>
 
 <style scoped>
+.detailnavbar{
+  position: relative;
+  z-index: 9;
+}
+ /*.swipper{
+  z-index: 10; 
+}*/
+.detail{
+  position: relative;
+  background-color: white;
+  z-index: 8;
+  height: 100vh;
+}
+.content1{
+  position:absolute;
+  top: 44px;
+  bottom: 0px;
+}
 </style>
